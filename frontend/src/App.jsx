5f86ba1dd6f5e5837ba2ceeec8e27ff217a1ad4a
aaljo2 <aaljo2@naver.com>
2025-08-31 17:47:10 +0900

    대대적인 수헞2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,38 @@
-
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import { AuthProvide } from './context/AuthContext'
-import { useEffect, useState } from 'react'
-import Loading from './components/Loading'
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Loading from "./components/Loading";
+import Navbar from "./components/Navbar";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000); 
+    }, 2000);
 
     // Cleanup timer
     return () => clearTimeout(timer);
   }, []);
 
   if (loading) {
-    return <Loading />; 
+    return <Loading />;
   }
-
 
   return (
     <>
-      <AuthProvide>
+      <AuthProvider>
         <Navbar />
-        <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+        <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
           <Outlet />
         </main>
         <Footer />
-      </AuthProvide>
-
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
